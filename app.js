69ecee6c9c6e79b7afb5490820f80854fe1c3f46
JunghwanYoon7 <junghwan.r.yoon@gmail.com>
2023-09-13 01:38:03 -0700

    1st page, icon showing

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-const section = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn = document.querySelectorAll('.control')
-const allSections = document.querySelectorAll('.main-content')
+// const section = document.querySelectorAll('.section');
+// const sectBtns = document.querySelectorAll('.controlls');
+// const sectBtn = document.querySelectorAll('.control');
+// const allSections = document.querySelectorAll('.main-content');
 
-function PageTransitions(){
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className= currentBtn[0].className.replace('active-btn', '');
-            this.className += 'active-btn';
-        })
-    }
+// function PageTransitions(){
+//     for(let i = 0; i < sectBtn.length; i++){
+//         sectBtn[i].addEventListener('click', function(){
+//             let currentBtn = document.querySelectorAll('.active-btn');
+//             currentBtn[0].className= currentBtn[0].className.replace('active-btn', '');
+//             this.className += 'active-btn';
+//         })
+//     }
 
-    //section active class
-    allSections.addEventListener('click', (e) => {
-        const id = e.target.dataset.id;
+//     //section active class
+//     allSections.addEventListener('click', (e) => {
+//         const id = e.target.dataset.id;
 
-        if(id){
-            //remove selected from other button
-            sectBtns.forEach((btn) =>{
-                btn.classList.remove('active')
-            })
+//         if(id){
+//             //remove selected from other button
+//             sectBtns.forEach((btn) =>{
+//                 btn.classList.remove('active-btn')
+//             })
 
-            e.target.classList.add('active')
+//             e.target.classList.add('active-btn')
 
-            sections.forEach((section) =>{
-                section.classList.remove('active')
-            })
+//             sections.forEach((section) =>{
+//                 section.classList.remove('active-btn')
+//             })
 
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
-    })
-}
+//             const element = document.getElementById(id);
+//             element.classList.add('active-btn');
+//         }
+//     })
+// }
 
-PageTransitions();
+// PageTransitions();
