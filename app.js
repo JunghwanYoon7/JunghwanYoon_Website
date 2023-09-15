66f936882ed1af3813b10636f665ef45e9be1d55
JunghwanYoon7 <junghwan.r.yoon@gmail.com>
2023-09-14 17:47:04 -0700

    section switcinng working

--- app.js
+++ app.js
@@ -1,37 +1,22 @@
-const section = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controlls');
-const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+// const sections = document.querySelectorAll('.section');
+// const sectBtns = document.querySelectorAll('.controlls');
+// const sectBtn = document.querySelectorAll('.control');
+// const allSections = document.querySelectorAll('.main-content');
 
-function PageTransitions(){
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className= currentBtn[0].className.replace('active-btn', '');
-            this.className += 'active-btn'
+
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
         })
-    }
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
 
-//     //section active class
-//     allSections.addEventListener('click', (e) => {
-//         const id = e.target.dataset.id;
-
-//         if(id){
-//             //remove selected from other button
-//             sectBtns.forEach((btn) =>{
-//                 btn.classList.remove('active-btn')
-//             })
-
-//             e.target.classList.add('active-btn')
-
-//             sections.forEach((section) =>{
-//                 section.classList.remove('active-btn')
-//             })
-
-//             const element = document.getElementById(id);
-//             element.classList.add('active-btn');
-//         }
-//     })
-}
 
 PageTransitions();
